47775aef3940c83d753146ef2c7c5c98b0cd9deb
lucasferreiralimax <lucasferreiralimax@gmail.com>
2020-12-29 16:50:09 -0800

    adjustment header float with app-views background grey

--- src/app/views/views.component.ts
+++ src/app/views/views.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation  } from '@angular/core';
 
 @Component({
   selector: 'app-views',
   templateUrl: './views.component.html',
-  styleUrls: ['./views.component.scss']
+  styleUrls: ['./views.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ViewsComponent implements OnInit {
 
