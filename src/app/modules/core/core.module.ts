43a39cba9bcf36908c10dbef4e216f89902de462
lucasferreiralimax <lucasferreiralimax@gmail.com>
2020-12-29 20:33:42 -0800

    remove bookmarks core module

--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -3,11 +3,10 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router'
 
 import { HeaderComponent } from '../../components/header/header.component';
-import { BookmarksComponent } from '../../components/bookmarks/bookmarks.component';
 
 @NgModule({
-  declarations: [HeaderComponent, BookmarksComponent],
-  exports: [HeaderComponent, BookmarksComponent],
+  declarations: [HeaderComponent],
+  exports: [HeaderComponent],
   imports: [
     CommonModule,
     RouterModule
