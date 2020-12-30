32bd3bfabbe273db41120690eeab553c44776e8b
lucasferreiralimax <lucasferreiralimax@gmail.com>
2020-12-29 20:34:33 -0800

    adjustment button bookmarks with responsive show and hidden

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Location } from '@angular/common';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -9,10 +12,16 @@ import { Location } from '@angular/common';
 })
 export class HeaderComponent implements OnInit {
 
+  menuBookmarks:Observable<boolean>;
+
   constructor(
     private router: Router,
-    private location: Location
-  ) { }
+    private location: Location,
+    public breakpointObserver: BreakpointObserver
+  ) {
+    this.menuBookmarks = breakpointObserver.observe('(min-width: 1100px)')
+      .pipe(map(result => !result.matches));
+  }
 
   ngOnInit(): void {
   }
