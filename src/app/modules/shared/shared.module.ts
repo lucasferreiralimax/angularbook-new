6dd9743244bfe60166a87bd7213c530f8b455f8d
lucasferreiralimax <lucasferreiralimax@gmail.com>
2021-01-04 16:09:28 -0300

    adjustmen feed and new component post

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DarkmodeComponent } from '../../components/darkmode/darkmode.component';
+import { PostComponent } from '../../components/post/post.component';
 
 @NgModule({
-  declarations: [DarkmodeComponent],
-  exports: [DarkmodeComponent],
+  declarations: [DarkmodeComponent, PostComponent],
+  exports: [DarkmodeComponent, PostComponent],
   imports: [
     CommonModule
   ]
--- src/app/views/feed/feed.module.ts
+++ src/app/views/feed/feed.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { FeedRoutingModule } from './feed-routing.module';
 import { FeedComponent } from './feed.component';
 import { BookmarksModule } from '../bookmarks/bookmarks.module';
+import { SharedModule } from '../../modules/shared/shared.module';
 
 @NgModule({
   declarations: [FeedComponent],
   imports: [
     CommonModule,
     FeedRoutingModule,
-    BookmarksModule
+    BookmarksModule,
+    SharedModule
   ]
 })
 export class FeedModule { }
