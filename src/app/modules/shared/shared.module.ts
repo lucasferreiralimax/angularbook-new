ac1d050c6000e64dd77708c612c64c9f049771de
lucasferreiralimax <lucasferreiralimax@gmail.com>
2021-01-06 19:19:44 -0300

    new component feed-list in feed router

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -3,17 +3,20 @@ import { CommonModule } from '@angular/common';
 
 import { DarkmodeComponent } from '../../components/darkmode/darkmode.component';
 import { PostComponent } from '../../components/post/post.component';
+import { FeedListComponent } from '../../components/feed-list/feed-list.component';
 import { ComplementaryComponent } from '../../components/complementary/complementary.component';
 
 @NgModule({
   declarations: [
     DarkmodeComponent,
     PostComponent,
+    FeedListComponent,
     ComplementaryComponent
   ],
   exports: [
     DarkmodeComponent,
     PostComponent,
+    FeedListComponent,
     ComplementaryComponent
   ],
   imports: [
