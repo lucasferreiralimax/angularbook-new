51ccceead8b96168c99653546ae766c7e7592fdf
lucasferreiralimax <lucasferreiralimax@gmail.com>
2021-01-06 20:47:19 -0300

    adjustment page login with form submit and services

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { FormBuilder, Validators, FormControl } from '@angular/forms'
+import { Router } from '@angular/router'
+import { LoginService } from '@services/login.service'
+import { NotificationService } from '@services/notification.service'
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm
+  loadingLogin = false
 
-  ngOnInit(): void {
+  constructor(
+    private loginService: LoginService,
+    private notificationService: NotificationService,
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {
+    this.loginForm = this.formBuilder.group({
+      email: ['lucasferreiralimax@gmail.com', [
+        Validators.required,
+        Validators.email
+      ]],
+      password: ['lucas', Validators.required]
+    })
   }
 
+  ngOnInit() {
+  }
+
+  onSubmitLogin(formData: any) {
+    this.loadingLogin = true
+    this.loginForm.controls['password'].reset()
+
+    this.loginService.loginUser(formData).subscribe(
+      (res: any) => {
+        this.loginService.validationSet(res)
+        this.loadingLogin = false
+        this.notificationService.notification(res.notification.type, res.notification.title, res.notification.content)
+        if(this.loginService.validation()) {
+          this.router.navigate(['/'])
+        }
+      },
+      (err: any) => {
+        this.loadingLogin = false
+        this.notificationService.notification("error", err.error, err.message)
+        console.log("error", err)
+      }
+    )
+  }
 }
--- src/app/views/login/login.module.ts
+++ src/app/views/login/login.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
-import { SharedModule } from '../../modules/shared/shared.module';
+import { SharedModule } from '@modules/shared/shared.module';
 
 @NgModule({
   declarations: [LoginComponent],
   imports: [
     CommonModule,
     LoginRoutingModule,
-    SharedModule
+    SharedModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class LoginModule { }
