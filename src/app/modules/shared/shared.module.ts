38109f27d3f70d9281aa9a4856816c3e9fa49161
lucasferreiralimax <lucasferreiralimax@gmail.com>
2021-01-07 14:20:38 -0300

    component loading with animation css

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -5,19 +5,22 @@ import { DarkmodeComponent } from '@components/darkmode/darkmode.component';
 import { PostComponent } from '@components/post/post.component';
 import { FeedListComponent } from '@components/feed-list/feed-list.component';
 import { ComplementaryComponent } from '@components/complementary/complementary.component';
+import { LoadingComponent } from '@components/loading/loading.component';
 
 @NgModule({
   declarations: [
     DarkmodeComponent,
     PostComponent,
     FeedListComponent,
-    ComplementaryComponent
+    ComplementaryComponent,
+    LoadingComponent
   ],
   exports: [
     DarkmodeComponent,
     PostComponent,
     FeedListComponent,
-    ComplementaryComponent
+    ComplementaryComponent,
+    LoadingComponent
   ],
   imports: [
     CommonModule
