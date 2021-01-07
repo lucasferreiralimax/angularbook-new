03180ca5051a23a90e09df8b98d68d527ee5a78f
lucasferreiralimax <lucasferreiralimax@gmail.com>
2021-01-07 03:48:03 -0300

    implementation button logout with role and aria-label

--- src/app/components/accounts/accounts.component.ts
+++ src/app/components/accounts/accounts.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginService } from '@services/login.service';
 
 @Component({
   selector: 'app-accounts',
@@ -9,9 +11,17 @@ export class AccountsComponent implements OnInit {
 
   @Input() show: boolean = false;
 
-  constructor() { }
+  constructor(
+    private loginService: LoginService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  loginLoggout() {
+    this.loginService.logout()
+    this.router.navigate(['login'])
   }
 
 }
