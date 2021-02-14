b07d8cd932ffc5b631e05e4e1f62fe9e637301bc
lucasferreiralimax <lucasferreiralimax@gmail.com>
2021-02-14 00:44:50 -0800

    import coreModule in profile

--- src/app/views/profile/profile.module.ts
+++ src/app/views/profile/profile.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { ProfileComponent } from './profile.component';
 import { ProfileRoutingModule } from './profile-routing.module';
 import { SharedModule } from '@modules/shared/shared.module';
+import { CoreModule } from '@modules/core/core.module';
 
 @NgModule({
   declarations: [ProfileComponent],
   imports: [
     CommonModule,
     ProfileRoutingModule,
-    SharedModule
+    SharedModule,
+    CoreModule
   ]
 })
 export class ProfileModule { }
