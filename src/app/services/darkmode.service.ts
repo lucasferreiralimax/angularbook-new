45f4b854b8bd99c29adcbbc5cc0bca78dcfde4a1
lucasferreiralimax <lucasferreiralimax@gmail.com>
2021-02-19 03:57:20 -0800

    new var filter icons

--- src/app/services/darkmode.service.ts
+++ src/app/services/darkmode.service.ts
@@ -19,9 +19,11 @@ export class DarkmodeService {
     localStorage.setItem("dark", JSON.stringify(type))
     if(type) {
       this.setColors(["#fff", "51, 51, 51", "51, 51, 51", "#999", "#eee"])
+      this.root.style.setProperty("--filter-icon", "invert(1)")
       this.root.querySelector('body').classList.add('dark-mode')
     } else {
       this.setColors(["#1c1e21", "255, 255, 255", "240, 242, 245", "#dddfe2", "#737373"])
+      this.root.style.setProperty("--filter-icon", "invert(59%) sepia(11%) saturate(200%) saturate(135%) hue-rotate(176deg) brightness(96%) contrast(94%)")
       this.root.querySelector('body').classList.remove('dark-mode')
     }
   }
