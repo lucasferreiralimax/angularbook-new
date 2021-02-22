f1e7278c3476ad0df09660afaeb110451c591168
lucasferreiralimax <lucasferreiralimax@gmail.com>
2021-02-21 21:59:46 -0800

    adjustment services in view profile and feed

--- src/app/views/feed/feed.component.ts
+++ src/app/views/feed/feed.component.ts
@@ -3,6 +3,9 @@ import { BreakpointObserver } from '@angular/cdk/layout';
 import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 
+import { UserService } from '@services/user.service'
+import { PostService } from '@services/post.service'
+
 @Component({
   selector: 'app-feed',
   templateUrl: './feed.component.html',
@@ -13,7 +16,12 @@ export class FeedComponent implements OnInit {
   asideBookmarks:Observable<boolean>;
   asideComplentary:Observable<boolean>;
 
-  constructor(public breakpointObserver: BreakpointObserver) {
+  user: any;
+  feed: any;
+
+  constructor(public breakpointObserver: BreakpointObserver,
+              private userService: UserService,
+              private postService: PostService) {
     this.asideBookmarks = breakpointObserver.observe('(max-width: 1100px)')
       .pipe(map(result => !result.matches));
     this.asideComplentary = breakpointObserver.observe('(max-width: 900px)')
@@ -21,6 +29,22 @@ export class FeedComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    if(this.userService.getUser()) {
+      this.user = this.userService.getUser()
+    } else {
+      this.user = this.userService.getUserMockado()
+    }
+    this.getFeed()
+  }
+
+  getFeed(): void {
+    this.postService.getListagem().subscribe(
+      res => this.feed = res,
+      error => {
+        console.log(error)
+        this.feed = this.postService.getListagemMock()
+      }
+    );
   }
 
 }
