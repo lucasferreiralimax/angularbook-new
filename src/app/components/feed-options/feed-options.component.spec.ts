5d9ecb29d4e9d29772071a675b3fd185736ed223
lucasferreiralimax <lucasferreiralimax@gmail.com>
2021-02-28 03:55:26 -0800

    test: update spec feed-options with httpModule

--- src/app/components/feed-options/feed-options.component.spec.ts
+++ src/app/components/feed-options/feed-options.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { FeedOptionsComponent } from './feed-options.component';
 
@@ -8,7 +9,8 @@ describe('FeedOptionsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FeedOptionsComponent ]
+      declarations: [ FeedOptionsComponent ],
+      imports: [ HttpClientTestingModule ]
     })
     .compileComponents();
   });
